01c1860cb9cb5878b8cf9ab7762ceb8bc68a5748
arupanandaswain <arupaswain7735@gmail.com>
2025-10-07 14:09:18 +0530

    updated aboutArray.go

--- goArray/aboutArray.go
+++ goArray/aboutArray.go
@@ -5,6 +5,16 @@ import (
 )
 
 func AcceptArray(){
+// defined length
     var storeArray = [5]int{1,2,4,5,7}
-    fmt.Printf("This is a %T and it stores %v", storeArray, storeArray)
+// inferred length
+    storeInferredArray := [...]int{2,32,3423,5,34,645,7,657,567}
+    storeStringInferredArray := [...]string{"arupa", "deni", "ashutosh", "subham", "i don't know"}
+    fmt.Printf("This is a %T and it stores %v\n", storeArray, storeArray)
+    fmt.Printf("This is a %T and it stores %v\n", storeInferredArray, storeInferredArray)
+    fmt.Printf("This is a %T and it stores %v\n", storeStringInferredArray, storeStringInferredArray)
+    fmt.Printf("the length is %v\n", len(storeStringInferredArray))
+    fmt.Printf("%v\n", storeStringInferredArray[0:3])
+    storeStringInferredArray[0] = "Arupa"
+    fmt.Printf("updated list : %v", storeStringInferredArray)
 }
