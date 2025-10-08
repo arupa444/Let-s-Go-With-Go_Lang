c769f7f6e1970fb6a24a1f0d0ef8556c1ba23610
arupanandaswain <arupaswain7735@gmail.com>
2025-10-08 16:15:50 +0530

    updated aboutArray.go

---
+++ goSwitch/aboutSwitch.go
@@ -0,0 +1,20 @@
+package goSwitch
+
+import (
+    "fmt"
+)
+
+func tier1Switch(ele){
+    switch ele {
+        case 1:
+            fmt.Println(ele,"reeeeeeeeee")
+        case 2:
+            fmt.Println(ele,"reeeeeeeeee")
+        case 3:
+            fmt.Println(ele,"reeeeeeeeee")
+        case 4:
+            fmt.Println(ele,"reeeeeeeeee")
+        default:
+            fmt.Println("there is no element name :",ele)
+    }
+}
