fddc65dda7a8626d9d1528f0cf97cf4b8cd057a8
arupanandaswain <arupaswain7735@gmail.com>
2025-10-16 13:36:46 +0530

    updated aboutMap.go

---
+++ goMap/aboutMap.go
@@ -0,0 +1,10 @@
+package goMap
+
+import (
+    "fmt"
+)
+
+func CreateMap(){
+    var storeCarPrices = map[string]int{"BMW":50000000, "Lambo": 80000000, "Suzuki": 3000000, "Toyota": 20000000, "RR": 2500000000}
+
+}
