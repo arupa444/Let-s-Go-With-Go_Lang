1fa0d0365b7c615e50e1c90c78a9262c12cdedf9
arupanandaswain <arupaswain7735@gmail.com>
2025-10-16 19:50:08 +0530

    updated aboutMap.go and main.go

--- goMap/aboutMap.go
+++ goMap/aboutMap.go
@@ -8,5 +8,7 @@ func CreateMap(){
     var storeCarPrices = map[string]int{"BMW":50000000, "Lambo": 80000000, "Suzuki": 3000000, "Toyota": 20000000, "RR": 2500000000}
     storeTeam := map[string]string{"Arupa": "AI", "Daniel": "AI"}
     fmt.Printf("%#v\n",storeCarPrices)
+    fmt.Printf("%v\n",storeCarPrices)
     fmt.Printf("%#v\n",storeTeam)
+    fmt.Printf("%v\n",storeTeam)
 }
