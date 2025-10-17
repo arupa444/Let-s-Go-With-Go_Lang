12aa3c5c70078327c9768f7ab52aebfc9cd88fda
arupanandaswain <arupaswain7735@gmail.com>
2025-10-17 11:21:42 +0530

    updated aboutMap.go and main.go

--- goMap/aboutMap.go
+++ goMap/aboutMap.go
@@ -4,11 +4,18 @@ import (
     "fmt"
 )
 
-func CreateMap(){
+func CreateStaticMap(){
     var storeCarPrices = map[string]int{"BMW":50000000, "Lambo": 80000000, "Suzuki": 3000000, "Toyota": 20000000, "RR": 2500000000}
     storeTeam := map[string]string{"Arupa": "AI", "Daniel": "AI"}
     fmt.Printf("%#v\n",storeCarPrices)
     fmt.Printf("%v\n",storeCarPrices)
     fmt.Printf("%#v\n",storeTeam)
     fmt.Printf("%v\n",storeTeam)
+}
+
+func CreateDynamicMap(){
+    var storeCarPrices = make(map[string]string)
+    storeCarPrices["name"] = "BMW"
+    storeCarPrices["price"] = "9000000"
+    storeCarPrices["location"] = "Berhampur"
 }
