6ad9e1c02cd07bbca25cc839543f104957bb4dd0
arupanandaswain <arupaswain7735@gmail.com>
2025-11-18 10:56:00 +0530

    update the goDirectory

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 import (
     "fmt"
+    "os"
+	"path/filepath"
+	"CLIPacks/Utils"
     "startGoLLM/inventoryDetails"
     "startGoLLM/goIfElse"
     "startGoLLM/goString"
@@ -198,6 +201,29 @@ func main(){
 //     goDirectory.RenameDir("huihui", "Satya")
 //     goDirectory.RemoveDir("Satya")
 //     goDirectory.RemoveNestedDir("huihui")
-    goDirectory.RenameDir("rtg", "go7For")
 
+    goDirectory.RenameDir("rtg", "go7For")
+	utils := "Utils"
+    err := os.MkdirAll(utils, 0755)
+    if err != nil{
+        fmt.Println("Error while creating a dict : ",err)
+        return
+    }
+
+    filePath := filepath.Join(utils, "try.go")
+    file, err := os.Create(filePath)
+    if err != nil{
+        fmt.Println("Error while creating a file : ",err)
+        return
+    }
+	defer file.Close()
+
+	_, err = file.WriteString("package trio\n\nimport(\n\t\"fmt\"\n)\n\nfunc Loon(){\nfmt.Println(\"Hey bro i coded it...\")\n}")
+	if err != nil{
+	    fmt.Println("Error while writing", err, file)
+	    return
+	}
+    fmt.Println("Created and wrote some syntax's on the create file")
+
+    trio.Loon()
 }
