1370bb4a2819b1bb53a9da5785127c22cbf8ded2
arupanandaswain <arupaswain7735@gmail.com>
2025-11-20 14:56:52 +0530

    added try.json and aboutJson.go in list

--- goJSON/aboutJson.go
+++ goJSON/aboutJson.go
@@ -5,12 +5,19 @@ import(
     "os"
 )
 
+type AccountDetailsStruct struct{
+    Balance float64
+    Currency string
+}
 
 type SavingsAccount struct{
-
+    Name string
+    AccountDetails AccountDetailsStruct
+    Age int
 }
 
 func FetchJsonData(){
+//     var saveAccount SavingsAccount
     jsonFile, err := os.Open("try.json")
     if err != nil{
         fmt.Errorf("The error : ", err)
